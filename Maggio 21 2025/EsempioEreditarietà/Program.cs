0daa64e28cce3a6593df67614f50c7f290fb8180
Simone Addesso <simoneaddesso25@gmail.com>
2025-05-21 12:57:34 +0200

    Add files via upload

---
+++ Maggio 21 2025/EsempioEreditarietà/Program.cs
@@ -0,0 +1,34 @@
+﻿// Classe base
+public class Animale
+{
+    public virtual void FaiVerso()
+    {
+        Console.WriteLine("L'animale fa un verso.");
+    }
+}
+
+// Classe derivata
+public class Cane : Animale
+{
+    // Metodo specifico della classe derivata
+    public void Scodinzola()
+    {
+        Console.WriteLine("Il cane scodinzola.");
+    }
+
+    public virtual void FaiVerso()
+    {
+        Console.WriteLine("Il cane dice bau.");
+    }
+}
+
+// Programma principale
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Cane mioCane = new Cane(); // Creazione di un oggetto della classe derivata
+        mioCane.FaiVerso(); // Metodo ereditato dalla classe base
+        mioCane.Scodinzola(); // Metodo definito nella classe derivata
+    }
+}
