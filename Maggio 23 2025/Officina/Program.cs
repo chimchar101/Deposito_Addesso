35670c35539de91f3ab5eb486f15e10fbde5fb52
Simone Addesso <simoneaddesso25@gmail.com>
2025-05-23 13:01:57 +0200

    Add files via upload

---
+++ Maggio 23 2025/Officina/Program.cs
@@ -0,0 +1,156 @@
+﻿public class Veicolo
+{
+    public string Targa
+    {
+        get;
+        set;
+    }
+
+    public virtual string Ripara()
+    {
+        return $"Il veicolo viene controllato.";
+    }
+}
+
+public class Auto : Veicolo
+{
+    public Auto(string targa)
+    {
+        Targa = targa;
+    }
+
+    public override string Ripara()
+    {
+        return $"Controllo olio, freni e motore dell'auto con targa \"{Targa}\".";
+    }
+}
+
+public class Moto : Veicolo
+{
+    public Moto(string targa)
+    {
+        Targa = targa;
+    }
+    
+    public override string Ripara()
+    {
+        return $"Controllo catena, freni e gomme della moto con targa \"{Targa}\".";
+    }
+}
+
+public class Camion : Veicolo
+{
+    public Camion(string targa)
+    {
+        Targa = targa;
+    }
+    
+    public override string Ripara()
+    {
+        return $"Controllo sospensioni, freni rinforzati e carico del camion con targa \"{Targa}\".";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool esci = false;
+        int scelta, budget = 0;
+        List<Veicolo> veicoli = new List<Veicolo>();
+
+        do
+        {
+            Console.WriteLine("Benvenuto. Cosa vuoi fare?");
+            Console.WriteLine("[1] Aggiungi Auto\n[2] Aggiungi Moto\n[3] Aggiungi Camion\n[4] Effettua un controllo su tutti i veicoli\n[0] Esci");
+            scelta = int.Parse(Console.ReadLine());
+
+            switch (scelta)
+            {
+                case 1:
+                    AggiungiAuto(veicoli);
+                    break;
+                case 2:
+                    AggiungiMoto(veicoli);
+                    break;
+                case 3:
+                    AggiungiCamion(veicoli);
+                    break;
+                case 4:
+                    RiparaVeicoli(veicoli, budget);
+                    break;
+                case 0:
+                    esci = true;
+                    Console.WriteLine("Arrivederci campione!");
+                    break;
+                default:
+                    Console.WriteLine("ERRORE INPUT - Ritorno al menù.");
+                    break;
+            }
+        } while (!esci);
+    }
+
+    private static void AggiungiAuto(List<Veicolo> veicoli)
+    {
+        Console.WriteLine("Inserisci la targa dell'auto:");
+        string targa = Console.ReadLine();
+
+        Auto auto = new Auto(targa);
+        veicoli.Add(auto);
+
+        Console.WriteLine("Auto inserita!");
+    }
+
+    private static void AggiungiMoto(List<Veicolo> veicoli)
+    {
+        Console.WriteLine("Inserisci la targa della moto:");
+        string targa = Console.ReadLine();
+
+        Moto moto = new Moto(targa);
+        veicoli.Add(moto);
+
+        Console.WriteLine("Moto inserita!");
+    }
+
+    private static void AggiungiCamion(List<Veicolo> veicoli)
+    {
+        Console.WriteLine("Inserisci la targa del camion:");
+        string targa = Console.ReadLine();
+
+        Camion camion = new Camion(targa);
+        veicoli.Add(camion);
+
+        Console.WriteLine("Camion inserito!");
+    }
+
+    private static void RiparaVeicoli(List<Veicolo> veicoli, int budget)
+    {
+        int contaRiparazioni = 0;
+
+        for (int i = 0; i < veicoli.Count; i++)
+        {
+            Console.WriteLine($"Veicolo {i + 1} | Targa: {veicoli[i].Targa}");
+        }
+
+        Console.WriteLine("Su quale veicolo vuoi andare a fare riparazioni? (Inserisci numero del veicolo)");
+        int v = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Quante riparazioni puoi fare? (Da 0 a 3)");
+        int r = int.Parse(Console.ReadLine());
+
+        if (r == 0)
+        {
+            Console.WriteLine("Non puoi effettuare riparazioni.");
+        }
+
+        else if (r >= 1 && r <= 3)
+        {
+            for (int i = 0; i < r; i++)
+            {
+                Console.WriteLine($"Riparazione {i + 1} su {r} | {veicoli[v - 1].Ripara()}");
+            }
+
+            Console.WriteLine("Riparazioni effettuate con successo!");
+        }
+    }
+}
---
+++ Maggio 23 2025/Operatori/Program.cs
@@ -0,0 +1,255 @@
+﻿public class Operatore
+{
+    private string _nome, _turno;
+
+    public string Nome
+    {
+        get;
+        set;
+    }
+
+    public string Turno
+    {
+        get
+        {
+            return _turno;
+        }
+        set
+        {
+            if (value.ToLower() == "giorno" || value.ToLower() == "notte")
+            {
+                _turno = value;
+            }
+
+            else
+            {
+                Console.WriteLine("Turno non valido.");
+            }
+        }
+    }
+
+    public virtual string Descrizione()
+    {
+        return $"Nome: {Nome} | Turno: {Turno}";
+    }
+
+    public virtual string EseguiCompito()
+    {
+        return "Operatore generico in servizio.";
+    }
+}
+
+public class OperatoreEmergenza : Operatore
+{
+    private int _livelloUrgenza;
+    public int LivelloUrgenza
+    {
+        get
+        {
+            return _livelloUrgenza;
+        }
+
+        set
+        {
+            if (value >= 1 && value <= 5)
+            {
+                _livelloUrgenza = value;
+            }
+
+            else
+            {
+                Console.WriteLine("Livello urgenza non valido.");
+            }
+        }
+    }
+
+    public OperatoreEmergenza(string nome, string turno, int livelloUrgenza)
+    {
+        Nome = nome;
+        Turno = turno;
+        LivelloUrgenza = livelloUrgenza;
+    }
+
+    public override string Descrizione()
+    {
+        return $"Operatore Emergenze | Nome: {Nome} | Turno: {Turno}";
+    }
+
+    public override string EseguiCompito()
+    {
+        return $"L'operatore {Nome} sta gestendo l'emergenza di livello {LivelloUrgenza}.";
+    }
+}
+
+public class OperatoreSicurezza : Operatore
+{
+    public string AreaSorvegliata
+    {
+        get;
+        set;
+    }
+
+    public OperatoreSicurezza(string nome, string turno, string areaSorvegliata)
+    {
+        Nome = nome;
+        Turno = turno;
+        AreaSorvegliata = areaSorvegliata;
+    }
+
+    public override string Descrizione()
+    {
+        return $"Operatore Sicurezza | Nome: {Nome} | Turno: {Turno}";
+    }
+
+    public override string EseguiCompito()
+    {
+        return $"L'operatore {Nome} sta sorvegliando {AreaSorvegliata}.";
+    }
+}
+
+public class OperatoreLogistica : Operatore
+{
+    private int _numeroConsegne;
+    public int NumeroConsegne
+    {
+        get
+        {
+            return _numeroConsegne;
+        }
+
+        set
+        {
+            if (value >= 0)
+            {
+                _numeroConsegne = value;
+            }
+
+            else
+            {
+                Console.WriteLine("Numero consegne non valido.");
+            }
+        }
+    }
+
+    public OperatoreLogistica(string nome, string turno, int numeroConsegne)
+    {
+        Nome = nome;
+        Turno = turno;
+        NumeroConsegne = numeroConsegne;
+    }
+
+    public override string Descrizione()
+    {
+        return $"Operatore Logistica | Nome: {Nome} | Turno: {Turno}";
+    }
+
+    public override string EseguiCompito()
+    {
+        return $"L'operatore {Nome} sta coordinando {NumeroConsegne} consegne.";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool esci = false;
+        int scelta;
+        List<Operatore> operatori = new List<Operatore>();
+
+        do
+        {
+            Console.WriteLine("Benvenuto. Cosa vuoi fare?");
+            Console.WriteLine("[1] Aggiungi Operatore Emergenze\n[2] Aggiungi Operatore Sicurezza\n[3] Aggiungi Operatore Logistico");
+            Console.WriteLine("[4] Controlla tutti gli operatori\n[5] Fai eseguire i compiti agli operatori\n[0] Esci");
+            scelta = int.Parse(Console.ReadLine());
+
+            switch (scelta)
+            {
+                case 1:
+                    AggiungiEmergenza(operatori);
+                    break;
+                case 2:
+                    AggiungiSicurezza(operatori);
+                    break;
+                case 3:
+                    AggiungiLogistica(operatori);
+                    break;
+                case 4:
+                    StampaOperatori(operatori);
+                    break;
+                case 5:
+                    Compito(operatori);
+                    break;
+                case 0:
+                    esci = true;
+                    Console.WriteLine("Arrivederci campione!");
+                    break;
+                default:
+                    Console.WriteLine("ERRORE INPUT - Ritorno al menù.");
+                    break;
+            }
+        } while (!esci);
+    }
+
+    private static void AggiungiEmergenza(List<Operatore> operatori)
+    {
+        Console.WriteLine("Inserisci il nome dell'operatore:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Che turno fa? (Giorno/Notte)");
+        string turno = Console.ReadLine();
+        Console.WriteLine("Livello dell'emergenza dell'operatore? (Da 1 a 5)");
+        int livello = int.Parse(Console.ReadLine());
+
+        OperatoreEmergenza operatore = new OperatoreEmergenza(nome, turno, livello);
+        operatori.Add(operatore);
+
+        Console.WriteLine("Operatore Emergenze inserito!");
+    }
+
+    private static void AggiungiSicurezza(List<Operatore> operatori)
+    {
+        Console.WriteLine("Inserisci il nome dell'operatore:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Che turno fa? (Giorno/Notte)");
+        string turno = Console.ReadLine();
+        Console.WriteLine("Dove sta sorvegliando?");
+        string area = Console.ReadLine();
+
+        OperatoreSicurezza operatore = new OperatoreSicurezza(nome, turno, area);
+        operatori.Add(operatore);
+
+        Console.WriteLine("Operatore Sicurezza inserito!");
+    }
+
+    private static void AggiungiLogistica(List<Operatore> operatori)
+    {
+        Console.WriteLine("Inserisci il nome dell'operatore:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Che turno fa? (Giorno/Notte)");
+        string turno = Console.ReadLine();
+        Console.WriteLine("Quante consegne sta coordinando?");
+        int consegne = int.Parse(Console.ReadLine());
+
+        OperatoreLogistica operatore = new OperatoreLogistica(nome, turno, consegne);
+        operatori.Add(operatore);
+
+        Console.WriteLine("Operatore Logistica inserito!");
+    }
+
+    private static void StampaOperatori(List<Operatore> operatori)
+    {
+        foreach (Operatore operatore in operatori)
+        {
+            Console.WriteLine(operatore.Descrizione());
+        }
+    }
+
+    private static void Compito(List<Operatore> operatori)
+    {
+        foreach (Operatore operatore in operatori)
+        {
+            Console.WriteLine(operatore.EseguiCompito());
+        }
+    }
+}
