2fe19e0115cb20a73789a7d776e098e33e3b0e6a
Simone Addesso <simoneaddesso25@gmail.com>
2025-05-29 18:31:38 +0200

    Add files via upload

---
+++ Maggio 29 2025/Bevande/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+
+public interface IBevanda
+{
+    string Descrizione();
+    double Costo();
+}
+
+public class Caffe : IBevanda
+{
+    public string Descrizione()
+    {
+        return "Caffè";
+    }
+
+    public double Costo()
+    {
+        return 1.20;
+    }
+}
+
+public class Te : IBevanda
+{
+    public string Descrizione()
+    {
+        return "Tè";
+    }
+
+    public double Costo()
+    {
+        return 2.50;
+    }
+}
+
+public abstract class DecoratoreBevanda : IBevanda
+{
+    protected IBevanda _bevanda;
+    
+    public DecoratoreBevanda(IBevanda bevanda)
+    {
+        _bevanda = bevanda;
+    }
+
+    public virtual string Descrizione()
+    {
+        return $"{_bevanda}";
+    }
+
+    public virtual double Costo()
+    {
+        return _bevanda.Costo();
+    }
+}
+
+public class ConLatte : DecoratoreBevanda
+{
+    public ConLatte(IBevanda bevanda)
+        : base(bevanda) { }
+
+    public override string Descrizione()
+    {
+        return $"{_bevanda.Descrizione()} con latte";
+    }
+
+    public override double Costo()
+    {
+        return _bevanda.Costo() + 0.25;
+    }
+}
+
+public class ConCioccolato : DecoratoreBevanda
+{
+    public ConCioccolato(IBevanda bevanda)
+        : base(bevanda) { }
+
+    public override string Descrizione()
+    {
+        return $"{_bevanda.Descrizione()} con cioccolato";
+    }
+
+    public override double Costo()
+    {
+        return _bevanda.Costo() + 1.00;
+    }
+}
+
+public class ConPanna : DecoratoreBevanda
+{
+    public ConPanna(IBevanda bevanda)
+        : base(bevanda) { }
+
+    public override string Descrizione()
+    {
+        return $"{_bevanda.Descrizione()} con panna";
+    }
+
+    public override double Costo()
+    {
+        return _bevanda.Costo() + 0.50;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool esci = false;
+        do
+        {
+            Console.WriteLine("Benvenuto. Che bevanda vuoi?");
+            Console.WriteLine("[1] Caffè\n[2] Tè\n[0] Esci");
+            string scelta = Console.ReadLine();
+
+            switch (scelta)
+            {
+                case "1":
+                    CreaCaffe();
+                    break;
+                case "2":
+                    CreaTe();
+                    break;
+                case "0":
+                    esci = true;
+                    Console.WriteLine("Arrivederci campione!");
+                    break;
+                default:
+                    Console.WriteLine("ERRORE INPUT - Ritorno al menù.");
+                    break;
+            }
+        } while (!esci);
+    }
+
+    private static void CreaCaffe()
+    {
+        IBevanda caffe = new Caffe();
+
+        Console.WriteLine("Che aggiunta vuoi fare?");
+        Console.WriteLine("[1] Latte\n[2] Cioccolato\n[3] Panna");
+        string sceltaB = Console.ReadLine();
+
+        switch (sceltaB)
+        {
+            case "1":
+                DecoratoreBevanda latte = new ConLatte(caffe);
+                Console.WriteLine($"Il tuo {latte.Descrizione()} verrà a costare {latte.Costo()} euro.");
+                Console.WriteLine("-----------");
+                break;
+            case "2":
+                DecoratoreBevanda cioccolato = new ConCioccolato(caffe);
+                Console.WriteLine($"Il tuo {cioccolato.Descrizione()} verrà a costare {cioccolato.Costo()} euro.");
+                Console.WriteLine("-----------");
+                break;
+            case "3":
+                DecoratoreBevanda panna = new ConPanna(caffe);
+                Console.WriteLine($"Il tuo {panna.Descrizione()} verrà a costare {panna.Costo()} euro.");
+                Console.WriteLine("-----------");
+                break;
+            default:
+                Console.WriteLine("ERRORE INPUT - Ritorno al menù.");
+                break;
+        }
+    }
+
+    private static void CreaTe()
+    {
+        IBevanda te = new Te();
+
+        Console.WriteLine("Che aggiunta vuoi fare?");
+        Console.WriteLine("[1] Latte\n[2] Cioccolato\n[3] Panna");
+        string sceltaB = Console.ReadLine();
+
+        switch (sceltaB)
+        {
+            case "1":
+                DecoratoreBevanda latte = new ConLatte(te);
+                Console.WriteLine($"Il tuo {latte.Descrizione()} verrà a costare {latte.Costo()} euro.");
+                Console.WriteLine("-----------");
+                break;
+            case "2":
+                DecoratoreBevanda cioccolato = new ConCioccolato(te);
+                Console.WriteLine($"Il tuo {cioccolato.Descrizione()} verrà a costare {cioccolato.Costo()} euro.");
+                Console.WriteLine("-----------");
+                break;
+            case "3":
+                DecoratoreBevanda panna = new ConPanna(te);
+                Console.WriteLine($"Il tuo {panna.Descrizione()} verrà a costare {panna.Costo()} euro.");
+                Console.WriteLine("-----------");
+                break;
+            default:
+                Console.WriteLine("ERRORE INPUT - Ritorno al menù.");
+                break;
+        }
+    }
+}
---
+++ Maggio 29 2025/CreazioneUtente/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+public interface IObserver
+{
+    void NotificaCreazione(string nomeUtente);
+}
+
+public interface ISubject
+{
+    void Registra(IObserver o);
+    void Rimuovi(IObserver o);
+    void Notifica(string nomeUtente);
+}
+
+public class GestoreCreazioneUtente : ISubject
+{
+    private readonly List<IObserver> _observer = new List<IObserver>();
+
+    public void Registra(IObserver osservatore)
+    {
+        _observer.Add(osservatore);
+    }
+
+    public void Rimuovi(IObserver osservatore)
+    {
+        _observer.Remove(osservatore);
+    }
+
+    public void Notifica(string nome)
+    {
+        foreach (var obs in _observer)
+        {
+            obs.NotificaCreazione(nome);
+        }
+    }
+
+    public void CreaUtente(string nome)
+    {
+        UserFactory.Crea(nome);
+        Notifica(nome);
+    }
+}
+
+public static class UserFactory
+{
+    public static Utente Crea(string nome)
+    {
+        return new Utente(nome);
+    }
+}
+
+public class Utente
+{
+    private string _nome;
+
+    public string Nome
+    {
+        get { return _nome; }
+        set { _nome = value; }
+    }
+
+    public Utente(string nome)
+    {
+        Nome = nome;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome utente: {Nome}";
+    }
+}
+
+public class ModuloLog : IObserver
+{
+    public void NotificaCreazione(string nomeUtente)
+    {
+        Console.WriteLine($"LOG | Creazione utente: {nomeUtente}");
+    }
+}
+
+public class ModuloMarketing : IObserver
+{
+    public void NotificaCreazione(string nomeUtente)
+    {
+        Console.WriteLine($"MARKETING | Creazione utente: {nomeUtente}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        GestoreCreazioneUtente gestore = new GestoreCreazioneUtente();
+        gestore.Registra(new ModuloLog());
+        gestore.Registra(new ModuloMarketing());
+        bool esci = false;
+        string news;
+
+        do
+        {
+            Console.WriteLine("Benvenuto. Cosa vuoi fare?");
+            Console.WriteLine("[1] Crea un nuovo utente\n[2] Stampa lista utenti\n[0] Esci");
+            string scelta = Console.ReadLine();
+
+            switch (scelta)
+            {
+                case "1":
+                    Console.WriteLine("Inserisci il nome dell'utente:");
+                    string nomeUtente = Console.ReadLine();
+                    gestore.CreaUtente(nomeUtente);
+                    break;
+                case "0":
+                    esci = true;
+                    Console.WriteLine("Arrivederci campione!");
+                    break;
+                default:
+                    Console.WriteLine("ERRORE INPUT - Ritorno al menù.");
+                    break;
+            }
+        } while (!esci);
+    }
+}
